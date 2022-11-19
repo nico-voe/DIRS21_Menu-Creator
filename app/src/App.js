c53219640fe364151978df4650bd62ff318382ad
Nico <96868692+nico-voe@users.noreply.github.com>
2022-11-19 16:13:30 +0100

    feat: react form hook added

--- app/src/App.js
+++ app/src/App.js
@@ -1,85 +1,11 @@
-import { useState } from "react";
 import "./App.css";
+
 import Form from "./components/Form";
 
-const App = () => {
-  const [values, setValues] = useState({});
-
-  const inputs = [
-    {
-      id: 1,
-      name: "name",
-      type: "text",
-      placeholder: "Dishname",
-      label: "Dishname",
-      required: true,
-    },
-    {
-      id: 2,
-      name: "description",
-      type: "text",
-      placeholder: "Description",
-      label: "Description",
-      required: true,
-    },
-    {
-      id: 3,
-      name: "price",
-      type: "text",
-      placeholder: "Price",
-      label: "Price",
-    },
-  ];
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const onChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
+export default function App() {
   return (
     <div className="app">
-      <form onSubmit={handleSubmit}>
-        <h1>Add Dish To Menu</h1>
-        {inputs.map((input) => (
-          <Form
-            key={input.id}
-            {...input}
-            value={values[input.name]}
-            onChange={onChange}
-          />
-        ))}{" "}
-        <div className="formInput">
-          <label>Category</label>
-          <select onChange={onChange}>
-            <option value="1">Starter</option>
-            <option value="2">Main Course</option>
-            <option value="3">Dessert</option>
-            <option value="4">Beverage</option>
-          </select>
-        </div>
-        <div className="formInput">
-          <label>Availability</label>
-          <select onChange={onChange}>
-            <option value="1">Breakfast</option>
-            <option value="2">Lunch</option>
-            <option value="3">Dinner</option>
-          </select>
-          <select onChange={onChange}>
-            <option value="1">Weekdays</option>
-            <option value="2">Weekends</option>
-          </select>
-        </div>
-        <div className="formInput">
-          <label>Waiting time in minutes</label>
-          <input type="number" min="0" max="100" onChange={onChange} />
-        </div>
-        <button>Submit</button>
-      </form>
+      <Form />
     </div>
   );
-};
-
-export default App;
+}
--- app/src/components/Form.jsx
+++ app/src/components/Form.jsx
@@ -1,11 +1,85 @@
 import { useState } from "react";
+import { useForm, useController } from "react-hook-form";
 import "./Form.css";
+import Select from "react-select";
 
-const Form = ({ label, onChange, id, ...inputProps }) => {
+const Form = () => {
+  const { register, control, handleSubmit } = useForm();
+
+  const { field } = useController(
+    { name: "category", control },
+    { name: "availability", control }
+  );
+
+  const apiPost = (formValues) => {
+    console.log("formValues", formValues);
+  };
+
+  const handleSave = (formValues) => {
+    apiPost(formValues);
+  };
+
+  const handleSelect = (option) => {
+    field.onChange(option.value);
+  };
+
+  const categories = [
+    { value: "Starter", label: "Starter" },
+    { value: "Main Course", label: "Main Course" },
+    { value: "Dessert", label: "Dessert" },
+    { value: "Beverage", label: "Beverage" },
+  ];
+  const availabilities = [
+    { value: "Breakfast", label: "Breakfast" },
+    { value: "Lunch", label: "Lunch" },
+    { value: "Dinner", label: "Dinner" },
+  ];
+
   return (
     <div className="formInput">
-      <label>{label}</label>
-      <input {...inputProps} onChange={onChange} />
+      <form className="formInput" onSubmit={handleSubmit(handleSave)}>
+        <h1>Add Dish To Menu</h1>
+
+        <div className="formInput">
+          <label>Dish Name</label>
+          <input {...register("name")} placeholder="Dish" />
+        </div>
+        <div className="formInput">
+          <label>Description</label>
+          <input {...register("description")} placeholder="Description" />
+        </div>
+        <div className="formInput">
+          <label>Price</label>
+          <input {...register("price")} placeholder="Price" />
+        </div>
+
+        <div className="formInput">
+          <label>Category</label>
+          <Select
+            className="select"
+            options={categories}
+            value={categories.find(({ value }) => value === field.value)}
+            onChange={handleSelect}
+          />
+        </div>
+
+        <div className="formInput">
+          <label>Available for</label>
+          <Select
+            className="select"
+            options={availabilities}
+            value={availabilities.find(({ value }) => value === field.value)}
+            onChange={handleSelect}
+          />
+        </div>
+
+        <div className="formInput">
+          <label>Waiting time in minutes</label>
+          <input {...register("number")} type="number" min="0" max="100" />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
